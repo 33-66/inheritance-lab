50d8cd11974e5782d4cacc78508c2abbf67f4a77
33-66 <simon.nganga@student.moringaschool.com>
2023-12-12 17:10:24 +0300

    commited work

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,15 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+    def learn(self,new_knowledge):
+        self.knowledge.append(new_knowledge)
+        
         pass
+
+simon =  Student("simon","doe")
+print(simon.first_name)
+print(simon.learn("Fishing is a new knowledge"))
+print(simon.knowledge)
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,24 @@
-#!/usr/bin/env python
-
-from user import User
-
 import random
+from user import User  # Assuming User class is defined in user.py
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
+    
 
     def teach(self):
+        print(random.choice(self.knowledge))
         pass
+
+simon = Teacher("love","bi")
+print(simon.teach())
